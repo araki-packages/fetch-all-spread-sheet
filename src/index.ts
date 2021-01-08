cdbbc23fd81b24f0e0397eb49440c6402dc704f3
ArakiTakaki <araki_takaki@yahoo.co.jp>
2021-01-08 14:01:16 +0900

    refactaring

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-const { GoogleSpreadsheet } = require('google-spreadsheet');
+const {GoogleSpreadsheet} = require('google-spreadsheet');
 
 interface ICredential {
   client_email: string;
@@ -9,7 +9,7 @@ const getValues = async (sheet: any, range?: any): Promise<any> => {
   const rows = await sheet.getRows();
   return rows.map((row: any) => {
     const headers = sheet.headerValues;
-    const record: {[key: string]: string | null } = {};
+    const record: {[key: string]: string | null} = {};
     headers.map((header: any) => {
       try {
         const value = row[header];
@@ -36,20 +36,24 @@ const loadSheet = async (id: string, credential: ICredential): Promise<any> => {
 
 const createGetAllSheet = async (id: string, credential: ICredential) => {
   const spreadSheet = await loadSheet(id, credential);
-
-  const bookSheets = await Promise.all(
-    Object.entries(spreadSheet.sheetsById).map(async ([key, value]) => {
-      const table: any = await getValues(value);
-      const title: any = (value as any).title;
-      return {
-        key,
-        title,
-        value: table,
-      };
-    })
-  );
-  const getSheet = <T>(sheetName: string): { key: string; title: string; value: T[]} | undefined => {
-    return bookSheets.find(sheet => sheet.title === sheetName);
+  const getSheet = <T>(sheetName: string): Promise<{key: string; title: string; value: T[]}> | undefined => {
+    return Object.entries(spreadSheet.sheetsById)
+      .map(([key, value]) => {
+        const title = (value as any).title as string;
+        return {
+          key,
+          title,
+          value,
+        }
+      })
+      .filter((val) => val.title === sheetName)
+      .map(async (val) => {
+        const table: any = await getValues(val.value);
+        return {
+          ...val,
+          value: table
+        }
+      })[0];
   };
   return getSheet;
 };
